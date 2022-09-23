0b9439a87ad51c602615feb825c6345759dc8f7b
pick <picknavin556@gmail.com>
2022-09-23 22:42:58 +0700

    Add gesture

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -11,6 +11,9 @@ struct OnboardingView: View {
     // MARK: - PROPERTY
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffet: CGFloat = 0
+    
     
     // MARK: - BODY
     var body: some View {
@@ -79,7 +82,7 @@ struct OnboardingView: View {
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffet + 80)
                         
                         Spacer()
                     }
@@ -100,19 +103,31 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x:buttonOffet)
+                        .gesture(
+                            DragGesture()
+                                .onChanged{ gesture in
+                                    if gesture.translation.width > 0 && buttonOffet <= buttonWidth - 80 {
+                                        buttonOffet = gesture.translation.width
+                                    }
+                                }
+                                .onEnded{ _ in
+                                    if buttonOffet > buttonWidth / 2 {
+                                        buttonOffet = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    }else{
+                                        buttonOffet = 0
+                                    }
+                                    
+                                }
+                        )//: GESTURE
                         
                         Spacer()
                     }//: HSTACK
-                    
-                    
-                    
+       
                     
                 }//: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
                 
             } //: VSTACK
